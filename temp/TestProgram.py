525d1f65a84dae54c6f010951c88d3d29753c05f
jtagaca <jtagaca@csub.edu>
2021-07-06 20:43:52 -0700

    save

--- temp/TestProgram.py
+++ temp/TestProgram.py
@@ -8,17 +8,11 @@ import unittest
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
-        output = list(map(lambda x: set(x), program.powerset([1, 2, 3])))
-        self.assertTrue(len(output) == 8)
-        self.assertTrue(set([]) in output)
-        self.assertTrue(set([1]) in output)
-        self.assertTrue(set([2]) in output)
-        self.assertTrue(set([1, 2]) in output)
-        self.assertTrue(set([3]) in output)
-        self.assertTrue(set([1, 3]) in output)
-        self.assertTrue(set([2, 3]) in output)
-        self.assertTrue(set([1, 2, 3]) in output)
-
+        phoneNumber = "1905"
+        expected = ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k",
+                    "1x0l", "1y0j", "1y0k", "1y0l", "1z0j", "1z0k", "1z0l"]
+        actual = program.phoneNumberMnemonics(phoneNumber)
+        self.assertEqual(actual, expected)
 
 
 if __name__ == '__main__':
--- temp/program.py
+++ temp/program.py
@@ -1,15 +1,40 @@
-def powerset(array):
+phoneLookUp = {
+    "0": ["0"],
+    "1": ["1"],
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"]
+}
+
+
+def phoneNumberMnemonics(phoneNumber):
+    # time complexity is O(4^n* N) where 4 is the maximum len of a digit in the digit hash map an d the
+    # n is the len of a input digit where N is the time it takes to create a list or concatinate it so we do it every time we
+    # hit the base case
     # Write your code here.
-	
-	# if i== to the len of array then we append it. 
-	# but the array should be unique
-	# 2^n
-	subset=[[]]
-	for element in array:
-		for i in range(len(subset)):
-			currentsubset=subset[i]
-			# appending a new array to the subset.
-			subset.append(currentsubset+[element])
-	
-	
-	return subset
+    currentMne = ["0"]*len(phoneNumber)
+    result = []
+    helperMne(0, currentMne, phoneNumber, result)
+    return result
+
+
+def helperMne(idx, currentMne, phoneNumber, result):
+    if idx == len(phoneNumber):
+        # we are checking for extra call and if so then we can append.
+        # we make a string out of a list
+        currentString = ''.join(currentMne)
+        result.append(currentString)
+    else:
+        # we recursively call the value to the current mne for every letter.
+        digit = phoneNumber[idx]
+        # we use idx to keep track of the digit that we are in and
+        # the recursion call
+        values = phoneLookUp[digit]
+        for value in values:
+            currentMne[idx] = value
+            helperMne(idx+1, currentMne, phoneNumber, result)
