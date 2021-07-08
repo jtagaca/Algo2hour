184997a25c932e62be1a8c981bbef9f546b02b8b
jtagaca <jtagaca@csub.edu>
2021-07-07 20:47:26 -0700

    save

--- temp/TestProgram.py
+++ temp/TestProgram.py
@@ -1,6 +1,9 @@
 # This file is initialized with a code version of this
 # question's sample test case. Feel free to add, edit,
 # or remove test cases in this file as you see fit!
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
 
 import program
 import unittest
@@ -8,12 +11,11 @@ import unittest
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
-        phoneNumber = "1905"
-        expected = ["1w0j", "1w0k", "1w0l", "1x0j", "1x0k",
-                    "1x0l", "1y0j", "1y0k", "1y0l", "1z0j", "1z0k", "1z0l"]
-        actual = program.phoneNumberMnemonics(phoneNumber)
+        stairs = 4
+        maxSteps = 2
+        expected = 5
+        actual = program.staircaseTraversal(stairs, maxSteps)
         self.assertEqual(actual, expected)
-
 
 if __name__ == '__main__':
     unittest.main()
--- temp/program.py
+++ temp/program.py
@@ -1,40 +1,15 @@
-phoneLookUp = {
-    "0": ["0"],
-    "1": ["1"],
-    "2": ["a", "b", "c"],
-    "3": ["d", "e", "f"],
-    "4": ["g", "h", "i"],
-    "5": ["j", "k", "l"],
-    "6": ["m", "n", "o"],
-    "7": ["p", "q", "r", "s"],
-    "8": ["t", "u", "v"],
-    "9": ["w", "x", "y", "z"]
-}
-
-
-def phoneNumberMnemonics(phoneNumber):
-    # time complexity is O(4^n* N) where 4 is the maximum len of a digit in the digit hash map an d the
-    # n is the len of a input digit where N is the time it takes to create a list or concatinate it so we do it every time we
-    # hit the base case
+def staircaseTraversal(height, maxSteps):
     # Write your code here.
-    currentMne = ["0"]*len(phoneNumber)
-    result = []
-    helperMne(0, currentMne, phoneNumber, result)
-    return result
+    # initialize result
+    result = [0] * (height+1)
+    # base cases
+    result[0] = 1
+    result[1] = 1
 
-
-def helperMne(idx, currentMne, phoneNumber, result):
-    if idx == len(phoneNumber):
-        # we are checking for extra call and if so then we can append.
-        # we make a string out of a list
-        currentString = ''.join(currentMne)
-        result.append(currentString)
-    else:
-        # we recursively call the value to the current mne for every letter.
-        digit = phoneNumber[idx]
-        # we use idx to keep track of the digit that we are in and
-        # the recursion call
-        values = phoneLookUp[digit]
-        for value in values:
-            currentMne[idx] = value
-            helperMne(idx+1, currentMne, phoneNumber, result)
+    for currentHeight in range(2, height+1):
+        step = 1
+        while step <= maxSteps and step <= height:
+            result[currentHeight] += result[currentHeight-step]
+            step += 1
+        print(result)
+    return result[height]
