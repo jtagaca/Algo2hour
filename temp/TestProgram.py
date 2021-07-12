1e5ebb22d2ffd11cefe6d5b60aefa1c4857400d0
jtagaca <jtagaca@csub.edu>
2021-07-11 21:46:06 -0700

    save

--- temp/TestProgram.py
+++ temp/TestProgram.py
@@ -2,17 +2,19 @@
 # question's sample test case. Feel free to add, edit,
 # or remove test cases in this file as you see fit!
 
+# This file is initialized with a code version of this
+# question's sample test case. Feel free to add, edit,
+# or remove test cases in this file as you see fit!
+
 import program
 import unittest
 
 
 class TestProgram(unittest.TestCase):
     def test_case_1(self):
-        characters = "aheaollabbhb"
-        document = "hello"
-        expected = True
-        actual = program.generateDocument(characters, document)
-        self.assertEqual(actual, expected)
+        self.assertEqual(program.longestPalindromicSubstring(
+            "abcdefghfedcbaa"), "aa")
+
 
 if __name__ == '__main__':
     unittest.main()
--- temp/program.py
+++ temp/program.py
@@ -1,14 +1,20 @@
-def generateDocument(characters, document):
-    # Write your code here.
-    h = {"": float("inf")}
-    for letter in characters:
-        if letter in h:
-            h[letter] += 1
-        else:
-            h[letter] = 1
-    for letter in document:
-        if letter in h:
-            h[letter] -= 1
-        else:
-            return False
-    return True
+def longestPalindromicSubstring(string):
+    # Write your code here
+    longest = [0, 1]
+    for i in range(1, len(string)):
+        odd = getPalindrome(i-1, i+1, string)
+        even = getPalindrome(i-1, i, string)
+        longestCurrentPalindrome = max(odd, even, key=lambda x: x[1]-x[0])
+        longest = max(longest, longestCurrentPalindrome,
+                      key=lambda x: x[1]-x[0])
+    return string[longest[0]:longest[1]]
+
+
+def getPalindrome(left, right, string):
+    while left >= 0 and right < len(string):
+        if string[left] != string[right]:
+            break
+        left -= 1
+        right += 1
+
+    return [left + 1, right]
